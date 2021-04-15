6d89c6142e3e667070b4618e066f95b939c1a850
Alan Alban <alan.alban10@gmail.com>
2021-04-15 11:53:51 -0400

    Updated DB data

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,16 @@
 const Sequelize = require("sequelize");
+const pkg = require("../../package.json");
 
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false, // unless you like the logs
-  // ...and there are many other options you may want to play with
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://Alan@localhost:5432/${databaseName}`,
+  {
+    logging: false, // unless you like the logs
+    // ...and there are many other options you may want to play with
+  }
+);
 
 module.exports = db;
